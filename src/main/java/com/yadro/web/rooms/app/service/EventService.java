7d72dcf02f216037956d2e1193d0bc89f0756c35
Ivan Zaichenko <i.v.zaichenko70@gmail.com>
2020-04-13 14:57:59 +0300

    Add hostel to event

--- src/main/java/com/yadro/web/rooms/app/service/EventService.java
+++ src/main/java/com/yadro/web/rooms/app/service/EventService.java
@@ -156,7 +156,8 @@ public class EventService {
 	    	//eventData.add("<a style=\"color: #f9b012\" href=\"/event/edit/" + event.getId() + "\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" width=\"24\" height=\"24\"><path d=\"M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25z\"></path><path d=\"M20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z\"></path></svg></a>");
 	    	eventData.add(event.getAccount().getUserName());
 	    	eventData.add(event.getRoom().getName());
-	    	eventData.add(event.getTitle());
+			eventData.add(event.getRoom().getHostel().getName());
+			eventData.add(event.getTitle());
 	    	eventData.add(String.valueOf(event.getStart()));
 	    	eventData.add(String.valueOf(event.getEnd()));
 	    	eventData.add("<a style=\"color: #f9b012\" href=\"/event/list/all/delete?id=" + event.getId() + "\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" width=\"24\" height=\"24\"><path d=\"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12z\"></path><path d=\"M19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z\"></path></svg></a>");
@@ -182,6 +183,7 @@ public class EventService {
 	    	eventData.add("<a style=\"color: #f9b012\" href=\"/event/edit/" + event.getId() + "\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" width=\"24\" height=\"24\"><path d=\"M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25z\"></path><path d=\"M20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z\"></path></svg></a>");
 	    	eventData.add(event.getTitle());
 	    	eventData.add(event.getRoom().getName());
+	    	eventData.add(event.getRoom().getHostel().getName());
 	    	eventData.add(String.valueOf(event.getStart()));
 	    	eventData.add(String.valueOf(event.getEnd()));
 	    	eventData.add("<a style=\"color: #f9b012\" href=\"/event/list/delete?id=" + event.getId() + "\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" width=\"24\" height=\"24\"><path d=\"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12z\"></path><path d=\"M19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z\"></path></svg></a>");
